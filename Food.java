f5b78c4d637cfdbcc07a82faeb943c5c4c0e6eaf
peanutchutney123 <55663746+peanutchutney123@users.noreply.github.com>
2020-04-10 14:01:11 -0500

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,18 @@
+package com.google.hangouts.javalearning;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by Kathir Kalyanaraman on 4/8/2020.
+ * Kathir Inc
+ * kathirrkalyan@gmail.com
+ */
+public class Food
+{
+    public String printMyFavoriteFood(String foodName){
+
+        String fruit = " Food name is  " + foodName;
+        return fruit;
+
+    }
+}
